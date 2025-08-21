a29dfe6e7fa7edc56fb60d61193443b858b11c0b
Jonathan <jonoproxd@gmail.com>
2025-08-22 04:32:06 +1200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({ theme }) => {
     <div className="bg-primary dark:bg-black pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40">
       {/* footer top */}
       <div className="flex justify-between lg:items-end max-lg:flex-col gap-10">
-        <div className="space-y-5 text-sm text-gray-600 dark:text-gray-500">
+        <div className="space-y-5 text-sm text-gray-600 dark:text-gray-400">
           <img
             src={
               theme === "dark"
@@ -55,7 +55,7 @@ const Footer = ({ theme }) => {
           </ul>
         </div>
 
-        <div className="text-gray-600 dark:text-gray-500">
+        <div className="text-gray-600 dark:text-gray-400">
           <h3 className="font-semibold">Join Our Mailing List</h3>
           <p className="text-sm mt-2 mb-6">
             The latest updates on my journey to bringing the best matcha to New
@@ -65,7 +65,7 @@ const Footer = ({ theme }) => {
             <input
               type="email"
               placeholder="Enter Your Email"
-              className="w-full p-3 text-sm outline-none rounded text-gray-800 dark:text-gray-200 bg-transparent border border-gray-300 dark:border-gray-500"
+              className="w-full p-3 text-sm outline-none rounded text-gray-800 dark:text-gray-300 bg-transparent border border-gray-300 dark:border-gray-400"
             />
             <button className="bg-gray-600 text-primary rounded px-6">
               Subscribe
@@ -76,7 +76,7 @@ const Footer = ({ theme }) => {
 
       <hr className="border-gray-300 dark:border-gray-600 my-6" />
       {/* footer bottom */}
-      <div className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap">
+      <div className="pb-6 text-sm text-gray-500 dark:text-gray-400 flex justify-center sm:justify-between gap-4 flex-wrap">
         <p>Copyright 2025 &copy; HouseOfBerry - All Right Reserved.</p>
         <div className="flex items-center justify-between gap-4">
           <img className="h-8" src={BrandAssets.fb} alt="Facebook Logo" />
@@ -84,7 +84,7 @@ const Footer = ({ theme }) => {
           <img className="h-8" src={BrandAssets.ln} alt="Linkdn Logo" />
           <img
             className="h-8"
-            src={BrandAssets.x_logo_black}
+            src={theme==='dark' ? BrandAssets.x_logo_white:BrandAssets.x_logo_black}
             alt="Twitter X Logo"
           />
         </div>
