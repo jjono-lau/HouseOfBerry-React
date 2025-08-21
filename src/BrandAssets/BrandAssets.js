ae743a4f9cf33d2959da025e1ff7c35adc9a015d
Jonathan <jonoproxd@gmail.com>
2025-08-22 04:14:40 +1200

    Footer Update
    Brand Assets Fixed

---
+++ src/BrandAssets/BrandAssets.js
@@ -0,0 +1,27 @@
+import fb from "./Facebook_logo_Primary.png";
+import fb_2 from "./Facebook_Logo_Secondary.png";
+import ln from './ln.png'
+import ln_black from "./ln-Black.png";
+import ln_white from "./ln-White.png";
+import ig_black from "./Instagram_Glyph_Black.svg";
+import ig_white from "./Instagram_Glyph_White.svg";
+import ig from "./Instagram_Glyph_Gradient.svg";
+import x_logo from "./x_logo.svg";
+import x_logo_black from "./x_logo-black.png";
+import x_logo_white from "./x_logo-white.png";
+
+const BrandAssets = {
+  fb,
+  fb_2,
+  ln,
+  ln_black,
+  ln_white,
+  ig_black,
+  ig_white,
+  ig,
+  x_logo,
+  x_logo_black,
+  x_logo_white,
+};
+
+export default BrandAssets;
